98e357d740f967e6bbfb5702ae014605361654b5
LucyHut <lucie.hutchins@yahoo.com>
2016-12-05 12:28:41 -0500

    Added a script to start blat server

---
+++ startGfServer.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/sh
+
+#
+#This script starts gfServer 
+# Only run the script if the gfServer is not running
+#
+# To check: ps -ef| grep gfServer
+#
+cd `dirname $0`/..
+LOG=`pwd`/startGfServer.log
+rm -f ${LOG}
+date | tee -a ${LOG}
+
+
+SERVER_NAME=`uname -n`
+GF_SERVER=`which gfServer`
+PORT=9038
+
+NIBDIR=/data/research/dna/mouse_build_38_nib
+LOG_FILE=/data/loads/mgi/mgigff/logs/gfServer.log
+# the set of chromosome nib files to load
+CHR="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 X Y MT"
+
+if echo "$GF_SERVER" | grep "not found">/dev/null; then
+  echo "gfServer Not Installed on $SERVER_NAME" | tee -a ${LOG}
+  exit 1
+fi
+
+if [ ! -d ${NIBDIR} ]
+then
+  echo "${NIBDIR} is not a directory" | tee -a ${LOG}
+  exit 1
+fi
+
+INPUTFILES=""
+# create the string of input files
+for c in ${CHR}
+do
+   INPUTFILES="${INPUTFILES} ${NIBDIR}/chr${c}.nib"
+done
+
+echo "gfServer is installed on $SERVER_NAME : $GF_SERVER" | tee -a ${LOG}
+echo "Starting blat server: $GF_SERVER start $SERVER_NAME $PORT ${INPUTFILES} -log=$LOG_FILE &" | tee -a ${LOG}
+$GF_SERVER start $SERVER_NAME $PORT ${INPUTFILES} -log=$LOG_FILE &
+
+if [ $? -ne 0 ]
+then
+  echo "gfServer $GF_SERVER failed to start on $SERVER_NAME" | tee -a ${LOG}
+  exit 1
+fi
+
+echo "gfServer $GF_SERVER started on $SERVER_NAME" | tee -a ${LOG}
+exit 0
